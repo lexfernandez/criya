2890fa45343fbe74ee97c41c06c926bdf838b121
lexfernandez <olorenzo@outlook.com>
2022-06-04 11:08:22 -0600

    feat: add criya colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    colors:{
+      transparent: 'transparent',
+      current: 'currentColor',
+      'pale-silver': '#CEC5B2',
+      'cultured': '#FAF8F4',
+      'white': '#ffffff',
+      'black': '#000000',
+      'outer-space': '#283E39 ',
+      'pear': '#c4dd2b ',
+      'platinum': '#E5E5E5',
+      'tropical-rain-forest': '#006063',
+    },
+    extend: {
+      fontFamily:{
+        'monserrat': 'Montserrat, sans-serif'
+      }
+    },
   },
   plugins: [],
 }
