2f53943e55af2c6d3a99f8b945b4ca2e38ef8832
lexfernandez <olorenzo@outlook.com>
2022-06-04 21:01:02 -0600

    fix: update stories due to thumbnails model change

--- stories/components/product.stories.tsx
+++ stories/components/product.stories.tsx
@@ -49,23 +49,23 @@ Default.args = {
         width: 1000,
         height: 664,
         url: "https://dl.airtable.com/.attachments/f026e3efd17cba2d1b9edfb73cd1d955/deb749bf/photo-1557374669-d1f478dc92c4ixlibrb-1.2.1ixideyJhcHBfaWQiOjEyMDd9autoformatfitcropw1000q80?ts=1654317990&userId=usrnsmHONxFRtHt8S&cs=3306ec42bd376ce4https://dl.airtable.com/.attachments/1d5da6ab7b5aaa85422470df41f4028f/41f6cfaf/photo-1543248939-4296e1fea89bixlibrb-1.2.1ixideyJhcHBfaWQiOjEyMDd9w1000q80?ts=1654356065&userId=usrnsmHONxFRtHt8S&cs=3f2b4d0e904d2d9b",
-        thumbnails: [
-          {
+        thumbnails: {
+          "small":{
             url: "https://dl.airtable.com/.attachmentThumbnails/0bfc00bcc317b91ede93541ac9992c6e/e2d95a38?ts=1654356065&userId=usrnsmHONxFRtHt8S&cs=a0208835cb3df269",
             width: 54,
             height: 36,
           },
-          {
+          "medium":{
             url: "https://dl.airtable.com/.attachmentThumbnails/7a3b89cd44914b9db86eb00357471871/e66c4135?ts=1654356065&userId=usrnsmHONxFRtHt8S&cs=dd5c184626ddce5c",
             width: 771,
             height: 512,
           },
-          {
+          "large":{
             url: "https://dl.airtable.com/.attachmentThumbnails/5bea028b2b1ffaa3f810d733ce86eea8/07c4361c?ts=1654356065&userId=usrnsmHONxFRtHt8S&cs=45e7382aa3abe89c",
             width: 3000,
             height: 3000,
           },
-        ],
+        },
       },
     ],
   },
