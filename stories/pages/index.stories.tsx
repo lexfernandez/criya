f7b59a3744a0029d6034028809ddade10f341842
lexfernandez <olorenzo@outlook.com>
2022-06-04 21:12:34 -0600

    test: mock main page requests

--- stories/pages/index.stories.tsx
+++ stories/pages/index.stories.tsx
@@ -1,12 +1,39 @@
-import { ComponentMeta, ComponentStory} from '@storybook/react'
-import Home from '../../pages'
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Product from "../../models/product.model";
+import Home from "../../pages";
+import { rest } from "msw";
+import furnitures from '../../fixtures/furnitures.json'
+const queryClient = new QueryClient();
 
 export default {
-    title: 'Pages/Home',
-    component: Home,
-    argTypes: {onClick: {action:true}}
+  title: "Pages/Home",
+  component: Home,
+  argTypes: { onClick: { action: true } },
+  decorators: [
+    (Story) => (
+      <QueryClientProvider client={queryClient}>
+        <Story />
+      </QueryClientProvider>
+    ),
+  ],
+  parameters:{
+    msw:{
+      handlers:{
+        furnitures: [
+          rest.get<Product[]>('*/api/furnitures',(req,res,ctx)=>{
+            return res(
+              ctx.status(200),
+              ctx.json(furnitures)
+            )
+          })
+        ]
+      }
+    }
+  }
 } as ComponentMeta<typeof Home>;
 
-const Template: ComponentStory<typeof Home> = (args) => <Home {...args} />
+const Template: ComponentStory<typeof Home> = (args) => <Home {...args} />;
 
 export const Default = Template.bind({});
+Default.args = {};
